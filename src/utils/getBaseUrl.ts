c0cbe7be4d61300097983fc0bd12385a97c39b12
paulpladziewicz <ppladziewicz@gmail.com>
2022-05-30 11:40:42 -0400

    added useful comments

--- src/utils/getBaseUrl.ts
+++ src/utils/getBaseUrl.ts
@@ -1,8 +1,10 @@
 export const getBaseUrl = () => {
   const protocol = window.location.protocol;
   const hostname = window.location.hostname;
+  // backend port
   const port = ':5003';
 
+  // production hostname
   if (hostname === 'freereaderslist') {
     return `${protocol}//${hostname}`;
   }
